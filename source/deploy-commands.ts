7713fe5b2b370cc37c6951bc62bbafedd807bbff
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-04 02:40:09 -0400

    Move away from guild commands

--- source/deploy-commands.ts
+++ source/deploy-commands.ts
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
-import { clientID, guildID, token } from '../config.json'
+import { clientID, token } from '../config.json'
 
 const commands = [
   new SlashCommandBuilder()
@@ -21,6 +21,6 @@ const commands = [
 const rest = new REST({ version: '9' }).setToken(token)
 
 rest
-  .put(Routes.applicationGuildCommands(clientID, guildID), { body: commands })
+  .put(Routes.applicationCommands(clientID), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error)
