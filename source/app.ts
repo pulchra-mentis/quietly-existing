e3c1d78a3a1bba082de20eb331766ed525e53ebb
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-04 01:32:43 -0400

    Prettify/lint/update package-lock.json

--- source/app.ts
+++ source/app.ts
@@ -3,27 +3,32 @@ import { Client, Intents, MessagePayload, TextBasedChannel } from 'discord.js'
 import { token } from '../config.json'
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 client.once('ready', () => {
-	console.log('Ready!');
-});
+  console.log('Ready!')
+})
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return
 
-	const { commandName } = interaction;
+  const { commandName } = interaction
 
-	if (commandName === 'quietly') {
-		const channel = await interaction.channel
-		const logChannel = await interaction.guild?.channels.cache.find(channel => channel.name === 'quietly-existing-logs') as TextBasedChannel
-		const message = await interaction.options.getString('message', true)
-		const user = await interaction.user.id
-		interaction.reply({ content: 'Your message is being sent anonymously, and may be logged to a separate channel if the server managers have configured this bot to do so.', ephemeral: true })
-		logChannel?.send(`<@${user}>:\n${message}`)
-		channel?.send(message)
-	}
+  if (commandName === 'quietly') {
+    const channel = await interaction.channel
+    const logChannel = (await interaction.guild?.channels.cache.find(
+      channel => channel.name === 'quietly-existing-logs',
+    )) as TextBasedChannel
+    const message = await interaction.options.getString('message', true)
+    const user = await interaction.user.id
+    interaction.reply({
+      content:
+        'Your message is being sent anonymously, and may be logged to a separate channel if the server managers have configured this bot to do so.',
+      ephemeral: true,
+    })
+    logChannel?.send(`<@${user}>:\n${message}`)
+    channel?.send(message)
+  }
+})
 
-});
-
-client.login(token);
+client.login(token)
--- source/deploy-commands.ts
+++ source/deploy-commands.ts
@@ -1,21 +1,26 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
-import { clientID, guildID, token } from "../config.json"
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+import { clientID, guildID, token } from '../config.json'
 
 const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription("Replies with pong!"),
-	new SlashCommandBuilder()
-		.setName('quietly')
-		.setDescription("Sends an anonymous message.")
-		.addStringOption(option =>
-			option.setName('message')
-				.setDescription('The message to anonymize.')
-				.setRequired(true))
-	].map(command => command.toJSON())
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with pong!'),
+  new SlashCommandBuilder()
+    .setName('quietly')
+    .setDescription('Sends an anonymous message.')
+    .addStringOption(option =>
+      option
+        .setName('message')
+        .setDescription('The message to anonymize.')
+        .setRequired(true),
+    ),
+].map(command => command.toJSON())
 
 const rest = new REST({ version: '9' }).setToken(token)
 
-rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest
+  .put(Routes.applicationGuildCommands(clientID, guildID), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error)
